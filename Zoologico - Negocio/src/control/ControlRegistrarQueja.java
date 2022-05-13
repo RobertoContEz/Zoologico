b21cf81c99edcf59c077400b47c842cf54ea5b5e
Edwing06 <105477451+Edwing06@users.noreply.github.com>
2022-05-13 00:27:22 -0700

    Cambios FabricaDAOS y controlQueja

--- Zoologico - Negocio/src/control/ControlRegistrarQueja.java
+++ Zoologico - Negocio/src/control/ControlRegistrarQueja.java
@@ -1,11 +1,31 @@
 package control;
 
+import implementacion.FabricaDAOs;
+import interfaces.IPersistenciaItinerario;
+import interfaces.IPersistenciaRecorrido;
+import java.util.List;
+import objetos.Recorrido;
+
 /**
  *
  * @author R.Pacheco, R.Contreras, E.Villagrana y G.Gaxiola
- * 
+ *
  * TODO: Documentar clase
  */
 public class ControlRegistrarQueja {
-    
+
+    public Recorrido recuperarRecorridos(String nombre) {
+        IPersistenciaRecorrido daoRecorrido = FabricaDAOs.getRecorridosDAO();
+        IPersistenciaItinerario daoItinerario = FabricaDAOs.getItinerariosDAO();
+        List<Recorrido> lista = daoRecorrido.consultarRecorridosUltimoMes();
+
+        for (Recorrido recorrido : lista) {
+            String nombreItinearario = daoItinerario.consultar(recorrido.getIdItinerario()).getNombre();
+            if (nombreItinearario.equals(nombre)) {
+                return recorrido;
+            }
+        }
+        return null;
+    }
+
 }
