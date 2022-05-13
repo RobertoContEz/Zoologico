1de9ea9586a07512677f7a6c752b61f96e025bed
Edwing06 <105477451+Edwing06@users.noreply.github.com>
2022-05-12 23:48:34 -0700

    Código controlRegistrarEspecie

--- Zoologico - Negocio/src/control/ControlRegistrarEspecie.java
+++ Zoologico - Negocio/src/control/ControlRegistrarEspecie.java
@@ -1,5 +1,14 @@
 package control;
 
+import implementacion.FabricaDAOs;
+import interfaces.IPersistenciaCuidador;
+import interfaces.IPersistenciaEspecie;
+import interfaces.IPersistenciaHabitat;
+import java.util.List;
+import objetos.Cuidador;
+import objetos.Especie;
+import objetos.Habitat;
+
 /**
  *
  * @author R.Pacheco, R.Contreras, E.Villagrana y G.Gaxiola
@@ -8,4 +17,51 @@ package control;
  */
 public class ControlRegistrarEspecie {
     
+    /**
+     * Recupera un lista con las hábitats registradas en la base de datos.
+     * @return lista con todas las hábitats registradas.
+     */
+    public List<Habitat> recuperaHabitats() {
+        IPersistenciaHabitat dao = FabricaDAOs.getHabitatsDAO();
+        
+        return dao.consultarTodos();
+    }
+    
+    /**
+     * Recupera un lista con los cuidadores registrados en la base de datos.
+     * @return lista con todos los cuidadores registrados.
+     */
+    public List<Cuidador> recuperaCuidadores() {
+        IPersistenciaCuidador dao = FabricaDAOs.getCuidadoresDAO();
+        
+        return dao.consultarTodos();
+    }
+    
+    /**
+     * Verifica la existencia del nombre en español de una especie.
+     * @param nombre parámetro a buscar en la base de datos.
+     * @return retorna la información de la especie en caso de estar registrada, null en caso contrario.
+     */
+    public Especie buscarEspecie(String nombre) {
+        IPersistenciaEspecie dao = FabricaDAOs.getEspeciesDAO();
+        
+        List<Especie> lista = dao.consultarTodos();
+        
+        for (Especie especie : lista) {
+            if(especie.getNombreEspanol().equals(nombre)) return especie;
+        }
+        
+        return null;
+    }
+    
+    /**
+     * Guarda un objeto de tipo especie en la base de datos.
+     * @param especie objeto a guardar en la base de datos.
+     * @return 
+     */
+    public boolean guardar(Especie especie) {
+        IPersistenciaEspecie dao = FabricaDAOs.getEspeciesDAO();
+        
+        return dao.agregar(especie);
+    }
 }
